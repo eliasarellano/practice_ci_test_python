2174aa6bd4bd387b0f177dfe1f0f222c419cb4f7
eliasarellano <elias.arellano@devoteam.com>
2022-02-20 14:08:39 +0100

    Testing syntax 4

--- choise.py
+++ choise.py
@@ -1,7 +1,7 @@
 import random
 
-words = ["rambo1","rambo2","rambo3",
-        "rambo4","rambo5","rambo6"]
+words = ["rambo1", "rambo2", "rambo3", 
+        "rambo4", "rambo5", "rambo6"]
 
 firstquestion = random.randint(0,5)
 secondquestion = firstquestion
@@ -12,7 +12,7 @@ while secondquestion == firstquestion:
 thirdquestion = firstquestion
 
 while thirdquestion == firstquestion or thirdquestion == secondquestion:
-  thirdquestion = random.randint(0,5)
+  thirdquestion = random.randint(0, 5)
 
 print("Firstquestion :")
 print(words[firstquestion])
@@ -21,13 +21,13 @@ print(words[secondquestion])
 print("Thirdquestion :")
 print(words[thirdquestion])
 
-#input file
+# input file
 fin = open("data.txt", "rt")
-#output file to write the result to
+# output file to write the result to
 fout = open("index.html", "wt")
-#for each line in the input file
+# for each line in the input file
 for line in fin:
     fout.write(line.replace('question1', words[firstquestion]).replace('question2', words[secondquestion]).replace('question3', words[thirdquestion]))
-#close input and output files
+# close input and output files
 fin.close()
 fout.close()
